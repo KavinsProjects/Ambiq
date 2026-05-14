7b7c196336abb9e749c0534c309454dbacce922c
KavinsProjects <kavinkanagaraj16@gmail.com>
2026-05-14 07:34:00 +0530

    Wroking on the App

---
+++ Ambiq/src/components/Ambiq.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './Ambiq.css'
+
+
+function Ambiq() {
+    return(
+        <div>
+                <div className="appContainer">
+                    <h3 className="heading">Ambiq</h3>
+
+                    <div className="imageandaudio">
+                        
+                    </div>
+                </div>
+
+        </div>
+    )
+}
+
+export default Ambiq;
